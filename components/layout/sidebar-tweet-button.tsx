5e40546e08101e6acfc02b60dcbc6409f0f69526
TEXTIL\gabriel.sousa <gabriel.sousa@tbmtextil.com.br>
2024-07-25 12:40:17 -0300

    install prisma

--- components/layout/sidebar-tweet-button.tsx
+++ components/layout/sidebar-tweet-button.tsx
@@ -1,10 +1,22 @@
+import { useLoginModal } from '@/hooks/useLoginModal'
+import { useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { FaFeather } from 'react-icons/fa'
 
 export const SidebarTweetButton = () => {
   const router = useRouter()
+  const loginModal = useLoginModal()
+
+  const onClick = useCallback(
+    () => {
+      loginModal.onOpen()
+      router.push('/')
+    },
+    [loginModal],
+  )
+  
   return (
-    <div onClick={() => router.push('/')}>
+    <div onClick={onClick}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
