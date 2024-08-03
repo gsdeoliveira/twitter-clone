3b4d288ea68a644a9dfb7cd41d55e2e447eb3c2f
gsdeoliveira <gabriielsousa2002@gmail.com>
2024-08-03 11:07:45 -0300

    updated profile and background image

--- components/users/user-hero.tsx
+++ components/users/user-hero.tsx
@@ -6,17 +6,20 @@ interface UserHeroProps {
   userId: string
 }
 
-
-
-export const UserHero: React.FC<UserHeroProps> = ({userId}) => {
-  const {data: fetchedUser } = useUser(userId)
+export const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+  const { data: fetchedUser } = useUser(userId)
   return (
     <div>
-      <div className='bg-neutral-700 h-44 relative'>
+      <div className="bg-neutral-700 h-44 relative">
         {fetchedUser?.coverImage && (
-          <Image src={fetchedUser.coverImage} fill alt='Cover Image' style={{objectFit: 'cover'}}  />
+          <Image
+            src={fetchedUser.coverImage}
+            fill
+            alt="Cover Image"
+            style={{ objectFit: 'cover' }}
+          />
         )}
-        <div className='absolute -bottom-16 left-4'>
+        <div className="absolute -bottom-16 left-4">
           <Avatar userId={userId} isLarge hasBorder />
         </div>
       </div>
